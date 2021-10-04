ad50430e30e4b6833f2d6492d6a952e8e7cc1fd9
Elankumaran98 <siva98kumarane@gmail.com>
2021-10-04 15:20:30 +0530

    unwanted files deleted

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-
-import { useState } from "react";
-import './App.css'
-import gypzeez from "./assets/gypzeez.png";
-
-import StripeContainer from "./components/stripecontainer";
 import StripeHome from "./components/stripehome";
 
-
 function App() {
-  const [showItem, setShowItem] = useState(false);
 
   return (
     <div className="App container">
-      <StripeHome/>
       <h1>Gypzeez payment</h1>
-      {showItem ? (
-        <StripeContainer />
-      ) : (
-        <>
-          <img src={gypzeez} alt="gypzeez" />
-          <button onClick={()=>{setShowItem(true)}}>Purchase Gypzeez</button>
-        </>
-      )}
+      <StripeHome/>
+      
     </div>
   );
 }
